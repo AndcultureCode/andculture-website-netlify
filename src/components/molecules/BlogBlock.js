cd05463035b92bc67a0aaa8abbfe4d5121f2bc1d
Shane Shearer <sshearer@andculture.com>
2019-09-19 07:19:45 -0400

    Adding two component shells to prepare for case study and blog blocks on the home page

---
+++ src/components/molecules/BlogBlock.js
@@ -0,0 +1,13 @@
+import React     from 'react'
+import PropTypes from 'prop-types';
+
+const BlogBlock = () => (
+  <div className="c-blog-block">
+
+  </div>
+)
+
+BlogBlock.propTypes = {
+};
+
+export default BlogBlock;
---
+++ src/components/molecules/CaseStudyBlock.js
@@ -0,0 +1,13 @@
+import React     from 'react'
+import PropTypes from 'prop-types';
+
+const CaseStudyBlock = () => (
+  <div className="c-casestudy-block">
+
+  </div>
+)
+
+CaseStudyBlock.propTypes = {
+};
+
+export default CaseStudyBlock;
