e22b20734914b7e6bf662c9bb6151f0575c55892
Phil Hess <phess@andculture.com>
2019-10-25 11:18:37 -0400

    Update HomeHeader to use headerText array

--- src/components/organisms/home/HomeHeader.js
+++ src/components/organisms/home/HomeHeader.js
@@ -6,14 +6,15 @@ import Menu                   from '../../molecules/Menu';
 import LogoMarkSVG            from '../../atoms/LogoMarkSVG';
 import { CSSTransitionGroup } from 'react-transition-group';
 
-const MainHeaderText = ({key, headerLine1, headerLine2, headerLine3, subtleText}) => (
+const MainHeaderText = ({key, headerText, subtleText}) => (
   <div className="header-text" key={key}>
     <h1>
-      <div className="header-text-main">{headerLine1}</div>
-      <div className="header-text-main">{headerLine2}</div>
-      <div className="header-text-main">{headerLine3}</div>
+      { headerText.map((text, key) => (<div className="header-text-main" key={key}>{text}</div>)) }
     </h1>
-    <label className="c-home-header__scroll header-text-subtle">{subtleText}</label>
+    {
+      subtleText != null &&
+        <label className="c-home-header__scroll header-text-subtle">{subtleText}</label>
+    }
   </div>
 )
 
@@ -34,32 +35,26 @@ const HomeHeader = () => {
   const headerLines = {
     default: (
       <MainHeaderText
-        key="default"
-        headerLine1={<>a<span className="fade">ndculture</span></>}
-        headerLine2="design"
-        headerLine3="company"
-        subtleText="Take a stroll, then scroll" />
+        key        = "default"
+        headerText = {[ <span>a<span className="fade">ndculture</span></span>, "design", "company" ]} />
     ),
     about: (
       <MainHeaderText
-        key="about"
-        headerLine1="process can"
-        headerLine2="be a dirty word"
-        subtleText="learn more about the andculture way" />
+        key        = "about"
+        headerText = {[ "process can", "be a dirty word" ]}
+        subtleText = "learn more about the andculture way" />
     ),
     blog: (
       <MainHeaderText
-        key="blog"
-        headerLine1="look into our"
-        headerLine2="world"
-        subtleText="check out our musings in the blog" />
+        key        = "blog"
+        headerText = {[ "look into our", "world" ]}
+        subtleText = "check out our musings in the blog" />
     ),
     work: (
       <MainHeaderText
-        key="blog"
-        headerLine1="we make it look"
-        headerLine2="like magic"
-        subtleText="learn more about who we help &amp; how" />
+        key        = "blog"
+        headerText = {[ "we make it look", "like magic" ]}
+        subtleText = "learn more about who we help &amp; how" />
     ),
   };
 
