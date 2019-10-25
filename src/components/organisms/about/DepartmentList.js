760f062fe631ab4bb63e736211cc3b9769240efc
Dylan Justice <djustice@andculture.com>
2019-10-25 12:04:36 -0400

    ACWEB-51 adding content and formatting code

--- src/components/organisms/about/DepartmentList.js
+++ src/components/organisms/about/DepartmentList.js
@@ -10,18 +10,39 @@ const DepartmentList = () => (
   <div className="c-about-department-list">
     <h2>how do you do what you do?</h2>
     <div className="c-about-department-list__item">
-      <img src={GeometricBlock} className="c-about-department-list__geometric-block" />
-      <DepartmentBlock name={"Strategy"} items={["item 1", "item 2", "item 3"]} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."} />
+      <img src       = {GeometricBlock}
+           className = "c-about-department-list__geometric-block" />
+      <DepartmentBlock 
+          name    = {"Strategy"}
+          items   = {["discovery and research", "data analysis", "content and marketing"]}
+          content = {"“Why?” That’s what our Strategy team intends to find out, right from Discovery on through site and content development. We dig deep to get to the “Why?” behind your team’s stated problem. We don’t assume an answer. But we’re ready to use data and expertise to explore solutions."} />
     </div>
     <div className="c-about-department-list__item">
-    <DepartmentBlock name={"Design"} items={["item 1", "item 2", "item 3"]} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."} />
+    <DepartmentBlock 
+        name    = {"Design"}
+        items   = {["product design", "web design", "event design"]}
+        content = {"From revamping a website to creating the aesthetics for a massive live event from scratch, our design team is up to the challenge. We value pushing each other creatively with a passion that borders on relentless. The result: your brand hits new levels."} />
     </div>
     <div className="c-about-department-list__item">
-      <img src={AndFreshCircle} className="c-about-department-list__fresh-circle" />
-      <img src={VioletStrokedSquare} className="c-about-department-list__violet-square" />
-      <DepartmentBlock name={"Engineering"} items={["item 1", "item 2", "item 3"]} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."} />
-      <img src={CenterLaneScribbles} className="c-about-department-list__scribbles" />
-      <img src={StickyNoteBrushSquares} className="c-about-department-list__brush-squares" />
+      <img src       = {AndFreshCircle}
+           className = "c-about-department-list__fresh-circle" />
+      <img src       = {VioletStrokedSquare}
+           className = "c-about-department-list__violet-square" />
+      <DepartmentBlock 
+          name    = {"Engineering"}
+          items   = {["app development", "software design", "client-side application"]}
+          content = {"Using Agile as our base, we work collaboratively with you to think beyond the obvious and deliver design thinking — it’s what has helped us come up with entirely new platforms out of thin air. You’re working with developers who live for this every day."} />
+
+      <img src       = {CenterLaneScribbles}
+           className = "c-about-department-list__scribbles" />
+      <img src       = {StickyNoteBrushSquares}
+           className = "c-about-department-list__brush-squares" />
+    </div>
+    <div className="c-about-department-list__item">
+      <DepartmentBlock
+          name    = {"Engagement"}
+          items   = {["client advising", "project management", "business development"]}
+          content = {"Organizing and understanding the nuances of a project can seem overwhelming. Our experienced engagement team works alongside you so you’re up to date as much as you want to be, from project updates to advising on your behalf in meetings so that your best interests are always represented in our designs."} />
     </div>
   </div>
 
