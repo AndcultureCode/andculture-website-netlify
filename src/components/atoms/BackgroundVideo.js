a0394d5fdb8117832a04d2ff09d63982df3a2240
Phil Hess <phess@andculture.com>
2019-10-25 12:27:57 -0400

    WIP Setting video times when loading/starting

--- src/components/atoms/BackgroundVideo.js
+++ src/components/atoms/BackgroundVideo.js
@@ -2,39 +2,67 @@ import React, { useRef }  from 'react'
 import PropTypes          from 'prop-types'
 import ReactPlayer        from "react-player";
 
-const BackgroundVideo = ({ className, playing, url }) => {
+const BackgroundVideo = ({ className, playing, onPause, seekTo, url }) => {
   const videoPlayerRef = useRef(null);
 
   if (!playing) {
     className += " -preload";
   }
 
-  const handlePlay = () => {
+  const handlePlay = (seekTo) => {
     if (videoPlayerRef != null && videoPlayerRef.current != null) {
-      videoPlayerRef.current.seekTo(0);
+      console.log("videoUrl" + url);
+      console.log("seek to: " + seekTo);
+      console.log("seek to setting: " + (seekTo || 0));
+      const reverseTime = seekTo == null ? 0 : videoPlayerRef.current.getDuration() - seekTo;
+      console.log("seek reverse time: " + reverseTime);
+      videoPlayerRef.current.seekTo(reverseTime);
     }
+  }
+
+  const handlePause = (state) => {
+    if (onPause == null) {
+      return;
+    }
+    console.log(state);
+    let currentTime = 0;
+    let totalTime = 0;
+    if (videoPlayerRef != null && videoPlayerRef.current != null) {
+      currentTime = videoPlayerRef.current.getCurrentTime();
+      totalTime = videoPlayerRef.current.getDuration();
+    }
+    if (currentTime == totalTime) {
+      currentTime = null;
+    }
+    onPause({
+      playedSeconds: currentTime
+    });
   }
 
   return (
     <ReactPlayer
-      className   = { className }
-      controls    = { false }
-      height      = "1080px"
-      muted       = { true }
-      onPlay      = { handlePlay }
-      playing     = { playing }
-      playsinline = { true }
-      ref         = { videoPlayerRef }
-      width       = "1300px"
-      url         = { url } />
+      className         = { className }
+      controls          = { false }
+      height            = "1080px"
+      muted             = { true }
+      onPlay            = { () => handlePlay(seekTo) }
+      playing           = { playing }
+      playsinline       = { true }
+      progressInterval  = { 100 }
+      onPause           = { handlePause }
+      ref               = { videoPlayerRef }
+      width             = "1300px"
+      url               = { url } />
   );
 }
 
 
 BackgroundVideo.propTypes = {
-  className: PropTypes.string,
-  playing:   PropTypes.bool,
-  url:       PropTypes.string,
+  className:  PropTypes.string,
+  playing:    PropTypes.bool,
+  onPause:    PropTypes.func,
+  seekTo:     PropTypes.number,
+  url:        PropTypes.string,
 }
 
 export default BackgroundVideo;
--- src/components/molecules/HomeCollage.js
+++ src/components/molecules/HomeCollage.js
@@ -1,5 +1,5 @@
 import PropTypes              from 'prop-types'
-import React                  from 'react'
+import React, { useState }    from 'react'
 import AngleBrushImage        from "../../img/home-collage_anglebrush@2x.png";
 import DotPatternImage        from "../../img/home-collage_dotpattern@2x.png";
 import DropsImage             from "../../img/home-collage_drops@2x.png";
@@ -16,30 +16,44 @@ import VioletAngleBrushImage  from "../../img/home-collage_violet_anglebrush@2x.
 import VioletLines            from "../../img/home-collage_violet_lines@2x.png";
 import BackgroundVideo        from "../atoms/BackgroundVideo";
 
-const HomeCollage = ({ activeVideo }) => (
-  <div className="c-home-collage">
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "default"} url="/video/default.mp4" />
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "about"} url="/video/about.mp4" />
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "aboutOut"} url="/video/aboutOut.mp4" />
-    {/* <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "blog"} url="/video/blog.mp4" />
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "blogOut"} url="/video/default.mp4" /> */}
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "contact"} url="/video/contact.mp4" />
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "contactOut"} url="/video/contactOut.mp4" />
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "work"} url="/video/work.mp4" />
-    <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "workOut"} url="/video/workOut.mp4" />
-    {/* <img src={LongBrushImage} alt="Yellow Long Brush Stroke" className="c-home-collage__longbrush" />
-    <img src={AngleBrushImage} alt="Angle Brush Stroke" className="c-home-collage__anglebrush" />
-    <img src={DropsImage} alt="Drops" className="c-home-collage__drops" />
-    <img src={VioletAngleBrushImage} alt="Violet Angle Brush" className="c-home-collage__violet__anglebrush" />
-    <img src={FreshBrushImage} alt="Fresh Brush" className="c-home-collage__fresh__brush" />
-    <img src={DotPatternImage} alt="Dot Pattern" className="c-home-collage__dotpattern" />
-    <img src={SquiggleBrushImage} alt="Squiggle Brush" className="c-home-collage__squigglebrush" />
-    <img src={SirachaAngleBrushImage} alt="Siracha Angle Brush" className="c-home-collage__siracha__anglebrush" />
-    <img src={TriangleImage} alt="Triangle" className="c-home-collage__triangle" />
-    <img src={RainbowImage} alt="Rainbow" className="c-home-collage__rainbow" />
-    <img src={VioletLines} alt="Violet Lines" className="c-home-collage__violet__lines" /> */}
-  </div>
-)
+const HomeCollage = ({ activeVideo }) => {
+  const [ videoSeconds,  setVideoSeconds ]  = useState(null);
+
+  const handleOnPause = (state) => {
+    console.log(state.playedSeconds);
+    if (state == null || state.playedSeconds == null) {
+      return;
+    }
+    setVideoSeconds(state.playedSeconds);
+  }
+
+  console.log(videoSeconds);
+
+  return (
+    <div className="c-home-collage">
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "default"} url="/video/default.mp4" />
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "about"} onPause={handleOnPause} url="/video/about.mp4" />
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "aboutOut"} onPause={handleOnPause} seekTo={videoSeconds} url="/video/aboutOut.mp4" />
+      {/* <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "blog"} url="/video/blog.mp4" />
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "blogOut"} url="/video/default.mp4" /> */}
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "contact"} onPause={handleOnPause} url="/video/contact.mp4" />
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "contactOut"} onPause={handleOnPause} seekTo={videoSeconds} url="/video/contactOut.mp4" />
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "work"} onPause={handleOnPause} url="/video/work.mp4" />
+      <BackgroundVideo className="c-home-collage__video" playing={activeVideo === "workOut"} onPause={handleOnPause} seekTo={videoSeconds} url="/video/workOut.mp4" />
+      {/* <img src={LongBrushImage} alt="Yellow Long Brush Stroke" className="c-home-collage__longbrush" />
+      <img src={AngleBrushImage} alt="Angle Brush Stroke" className="c-home-collage__anglebrush" />
+      <img src={DropsImage} alt="Drops" className="c-home-collage__drops" />
+      <img src={VioletAngleBrushImage} alt="Violet Angle Brush" className="c-home-collage__violet__anglebrush" />
+      <img src={FreshBrushImage} alt="Fresh Brush" className="c-home-collage__fresh__brush" />
+      <img src={DotPatternImage} alt="Dot Pattern" className="c-home-collage__dotpattern" />
+      <img src={SquiggleBrushImage} alt="Squiggle Brush" className="c-home-collage__squigglebrush" />
+      <img src={SirachaAngleBrushImage} alt="Siracha Angle Brush" className="c-home-collage__siracha__anglebrush" />
+      <img src={TriangleImage} alt="Triangle" className="c-home-collage__triangle" />
+      <img src={RainbowImage} alt="Rainbow" className="c-home-collage__rainbow" />
+      <img src={VioletLines} alt="Violet Lines" className="c-home-collage__violet__lines" /> */}
+    </div>
+  );
+}
 
 HomeCollage.propTypes = {
   activeVideo: PropTypes.string,
