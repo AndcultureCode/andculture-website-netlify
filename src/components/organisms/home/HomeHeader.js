a58535096465301f71337f960be873cbc2e48b4c
Michael Tyson <michaeltyson@outlook.com>
2019-10-25 07:05:26 -0400

    Animate main text on home screen

--- src/components/organisms/home/HomeHeader.js
+++ src/components/organisms/home/HomeHeader.js
@@ -1,12 +1,19 @@
-import React, { useState }  from 'react';
-import IconGrid             from '../../molecules/IconGrid';
-import SquiggleLine         from '../../atoms/SquiggleLine';
-import HomeCollage          from '../../molecules/HomeCollage';
-import Menu                 from '../../molecules/Menu';
-import LogoMarkSVG          from '../../atoms/LogoMarkSVG';
+import React, { useState }    from 'react';
+import IconGrid               from '../../molecules/IconGrid';
+import SquiggleLine           from '../../atoms/SquiggleLine';
+import HomeCollage            from '../../molecules/HomeCollage';
+import Menu                   from '../../molecules/Menu';
+import LogoMarkSVG            from '../../atoms/LogoMarkSVG';
+import { CSSTransitionGroup } from 'react-transition-group';
+
+const MainHeaderText = ({key, children}) => (
+  <div className="header-text" key={key}>
+    {children}
+  </div>
+)
 
 const HomeHeader = () => {
-  const [ activeIcon, setActiveIcon ] = useState(null);
+  const [ activeIcon,  setActiveIcon ]  = useState(null);
   const [ activeVideo, setActiveVideo ] = useState("default");
 
   const handleOnHover = (e, type) => {
@@ -19,31 +26,32 @@ const HomeHeader = () => {
     setActiveVideo(type);
   }
 
-  let header = [
-    (<h1 key="0">a<span className="fade">ndculture</span><br />design<br />company</h1>),
-    (<label key="1" className="c-home-header__scroll">Take a stroll, then scroll</label>)
-  ]
-
-  if (activeIcon === "about") {
-    header = [
-      (<h1 key="0">process can<br />be a dirty word</h1>),
-      (<label key="1" className="c-home-header__scroll">learn more about the andculture way</label>)
-    ]
-  }
-
-  if (activeIcon === "blog") {
-    header = [
-      (<h1 key="0">look into our<br />world</h1>),
-      (<label key="1" className="c-home-header__scroll">check out our musings in the blog</label>)
-    ]
-  }
-
-  if (activeIcon === "work") {
-    header = [
-      (<h1 key="0">we make it look<br />like magic</h1>),
-      (<label key="1" className="c-home-header__scroll">learn more about who we help &amp; how</label>)
-    ]
-  }
+  const headerLines = {
+    "default": (
+      <MainHeaderText key="default">
+          <h1>a<span className="fade">ndculture</span><br />design<br />company</h1>
+          <label className="c-home-header__scroll">Take a stroll, then scroll</label>
+      </MainHeaderText>
+    ),
+    "about": (
+      <MainHeaderText key="about">
+        <h1>process can<br />be a dirty word</h1>
+        <label className="c-home-header__scroll">learn more about the andculture way</label>
+      </MainHeaderText>
+    ),
+    "blog": (
+      <MainHeaderText key="blog">
+        <h1>look into our<br />world</h1>
+        <label className="c-home-header__scroll">check out our musings in the blog</label>
+      </MainHeaderText>
+    ),
+    "work": (
+      <MainHeaderText key="work">
+        <h1>we make it look<br />like magic</h1>
+        <label className="c-home-header__scroll">learn more about who we help &amp; how</label>
+      </MainHeaderText>
+    ),
+  };
 
   return (
     <header className="c-home-header">
@@ -51,7 +59,12 @@ const HomeHeader = () => {
       <HomeCollage activeVideo={activeVideo} />
       {/* <SquiggleLine /> */}
       <aside className="c-home-header__content">
-        { header.map((line) => line)}
+        <CSSTransitionGroup
+          transitionName="header-text"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={1000}>
+          { headerLines[activeIcon] || headerLines.default }
+        </CSSTransitionGroup>
         {/* <Menu /> */}
       </aside>
       <IconGrid onHover={handleOnHover} />
