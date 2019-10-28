3176c93c28f429d04facff7dacfe7b18f4065b24
Phil Hess <phess@andculture.com>
2019-10-28 13:54:25 -0400

    Add home header text animation

--- src/components/organisms/home/HomeHeader.js
+++ src/components/organisms/home/HomeHeader.js
@@ -5,17 +5,18 @@ import HomeCollage            from '../../molecules/HomeCollage';
 import Menu                   from '../../molecules/Menu';
 import LogoMarkSVG            from '../../atoms/LogoMarkSVG';
 import { CSSTransitionGroup } from 'react-transition-group';
+import Fade                   from "react-reveal/Fade";
 
-const MainHeaderText = ({key, headerText, subtleText}) => (
-  <div className="header-text" key={key}>
-    <h1>
-      { headerText.map((text, key) => (<div className="header-text-main" key={key}>{text}</div>)) }
-    </h1>
-    {
-      subtleText != null &&
-        <label className="c-home-header__scroll header-text-subtle">{subtleText}</label>
-    }
-  </div>
+const MainHeaderText = ({isVisible, headerText, subtleText}) => (
+  <Fade bottom cascade when = { isVisible }>
+    <div className = "header-text">
+      { headerText.map((text, key) => (<h1 key={key}>{text}</h1>)) }
+      {
+        subtleText != null &&
+          <label className="c-home-header__scroll header-text-subtle">{subtleText}</label>
+      }
+    </div>
+  </Fade>
 )
 
 const HomeHeader = () => {
@@ -32,41 +33,24 @@ const HomeHeader = () => {
     setActiveVideo(type);
   }
 
-  const headerLines = {
-    default: (
-      <MainHeaderText
-        key        = "default"
-        headerText = {[ <span>a<span className="fade">ndculture</span></span>, "design", "company" ]} />
-    ),
-    about: (
-      <MainHeaderText
-        key        = "about"
-        headerText = {[ "process can", "be a dirty word" ]} />
-    ),
-    contact: (
-      <MainHeaderText
-        key        = "contact"
-        headerText = {[ "yes, we'd love", "to talk" ]} />
-    ),
-    work: (
-      <MainHeaderText
-        key        = "work"
-        headerText = {[ "how we make it", "look like magic" ]} />
-    ),
-  };
-
   return (
     <header className="c-home-header">
       <LogoMarkSVG className="c-home-header__logo" />
       <HomeCollage activeVideo={activeVideo} />
       {/* <SquiggleLine /> */}
       <aside className="c-home-header__content">
-        <CSSTransitionGroup
-          transitionName="header-text-main"
-          transitionEnterTimeout={200}
-          transitionLeaveTimeout={300}>
-          { headerLines[activeIcon] || headerLines.default }
-        </CSSTransitionGroup>
+        <MainHeaderText
+          isVisible = { activeIcon == null }
+          headerText = {[ <span>a<span className="fade">ndculture</span></span>, "design", "company" ]} />
+        <MainHeaderText
+          isVisible = { activeIcon === "about" }
+          headerText = {[ "process can", "be a dirty word" ]} />
+        <MainHeaderText
+          isVisible = { activeIcon === "contact" }
+          headerText = {[ "yes, we'd love", "to talk" ]} />
+        <MainHeaderText
+          isVisible = { activeIcon === "work" }
+          headerText = {[ "how we make it", "look like magic" ]} />
         {/* <Menu /> */}
       </aside>
       <IconGrid onHover={handleOnHover} />
